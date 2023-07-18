9760493b2e004a1ee830e2e7d60c9096a785993e
Bruno120Ab <abreubruno499@gmail.com>
2023-07-18 10:42:17 -0300

    fix: resolve bugs

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,92 @@
+import { Container, Flex } from "@/styles/Global";
+import { Text } from "@/styles/Text";
+import { Button } from "@/styles/Buttons";
+
+import { Stack } from "@/components/Stack";
+import { Project } from "@/components/Project";
+import { Contacts } from "@/components/Contacts";
+
+import { stackData } from "@/utils/stackData";
+import { userData } from "@/utils/userData";
+
+import { FaGithub } from "react-icons/fa";
+
+import {
+  Header,
+  HeaderContent,
+  HeaderButtonsArea,
+  UserImage,
+  StackCards,
+  ProjectsArea,
+  ProjectsAreaSocialMediaMessage,
+  ProjectAreaWrapperColumns,
+  ProjectsAreaContent,
+} from "./style";
+
+export const Home = (): JSX.Element => {
+  const gihubUrl = `https://github.com/${userData.githubUser}`;
+  // const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+
+  return (
+    <main id="home">
+      <Header>
+        <Container>
+          <HeaderContent>
+            <Text as="h1" type="heading1" color="grey5">
+            Sou {" "}
+              <Text as="span" type="heading1" color="brand1">
+                Engenheiro
+              </Text>{" "}
+              de Software,{" "}
+              <Text as="span" type="heading1" color="brand1">
+                Desenvolvedor
+              </Text>{" "}
+            FullStack,Front-End, Back-end.
+            </Text>
+            <HeaderButtonsArea>
+              <Button as="a" type="primary" href="#projects">
+                Ver meus projetos
+              </Button>
+              <Button
+                color="grey5"
+                as="a"
+                css={{ "&:hover": { color: "$grey1" } }}
+                type="circle"
+                target="_blank"
+                href={gihubUrl}
+              >
+                <FaGithub />
+              </Button>
+            </HeaderButtonsArea>
+            <StackCards>
+              {stackData.map((stack, index) => (
+                <Stack key={index} title={stack.title} icon={stack.img} />
+              ))}
+            </StackCards>
+          </HeaderContent>
+        </Container>
+      </Header>
+      <ProjectsArea id="projects">
+        <Container>
+          <ProjectAreaWrapperColumns>
+            <ProjectsAreaSocialMediaMessage>
+              <Text as="h2" type="heading4" color="brand1">
+                Meus projetos
+              </Text>
+              <Text as="p" type="body1" color="grey2">
+                Alguns do meus {" "}
+                <Text as="span" color="brand5">
+                  projetos paralelos
+                </Text>
+              </Text>
+            </ProjectsAreaSocialMediaMessage>
+            <ProjectsAreaContent>
+              <Project />
+            </ProjectsAreaContent>
+          </ProjectAreaWrapperColumns>
+        </Container>
+      </ProjectsArea>
+      <Contacts />
+    </main>
+  );
+};
---
+++ src/utils/userData.ts
@@ -0,0 +1,7 @@
+export const userData = {
+  nameUser: "Bruno Abreu",
+  githubUser: "Bruno120Ab",
+  linkedinUser: "brunoabreudev",
+  whatsappNumber: "+71999099688",
+  emailUser: "brunabredevs@gmasil.com",
+};
